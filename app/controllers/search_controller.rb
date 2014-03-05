60cd9d9da0175b763620d86d6d3c2f1aea2405b1
Jack Bryant <jackbryant597@gmail.com>
2014-03-05 14:48:29 +0000

    user can now search for artist and if tracks available returns them as iframe with dl button

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,8 @@
 class SearchController < ApplicationController
 
-	# curl -v 'http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/fuck-vic-mensa&client_id=d8b21e837485a3086c3f20da86fee214'
-	# client_id = d8b21e837485a3086c3f20da86fee214
-
 	require 'soundcloud'
+	require 'net/http'
+	require 'json'
 
 	def new
 		@search = Search.new
@@ -15,14 +14,31 @@ class SearchController < ApplicationController
 	end
 
 	def index
+		@client = Soundcloud.new(client_id: 'd8b21e837485a3086c3f20da86fee214')
 		@search = Search.last
 		@search_result = @search.content
-		@client = Soundcloud.new(client_id: 'd8b21e837485a3086c3f20da86fee214')
-		@user_tracks = @client.get('/users/28508171/tracks')
+		uri = URI("http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/#{@search_result}&client_id=d8b21e837485a3086c3f20da86fee214")
+
+		Net::HTTP.start(uri.host, uri.port) do |http|
+  			request = Net::HTTP::Get.new uri
+
+  			@res = http.request request # Net::HTTPResponse object
+		end
+		@res
+		@res_json = @res.to_json
+		@res_hash = JSON.parse(@res_json)
+		@res_url = @res_hash["location"]
+		@res_url_string = @res_url.to_s
+		@res_url_1 = @res_url_string.gsub(/(("http:\/\/api.soundcloud.com\/users\/))/, "")
+		@res_url_2 = @res_url_1.gsub("?", "")
+		@res_url_3 = @res_url_2.gsub(/(.jsonclient_id=d8b21e837485a3086c3f20da86fee214"])/, "")
+		@user_id = @res_url_3.gsub("[", "")
+		@user_tracks = @client.get("/users/#{@user_id}/tracks")
 	end
  
 	private
  	def search_params
     	params.require(:search).permit(:content)
   	end
+  	
 end
